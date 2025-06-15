130a1f773ad62991c82496e648499d2dcc688d74
yiyaang <yiyaaang@gmail.com>
2025-06-16 02:35:35 +0900

    음 들여쓰기 바꼈나

--- generate_index.sh
+++ generate_index.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# index.html 헤더 및 <ul> 시작
 cat > index.html <<EOF
 <!DOCTYPE html>
 <html lang="ko">
@@ -8,13 +10,15 @@ cat > index.html <<EOF
   <ul>
 EOF
 
+# images/ 폴더의 파일명을 <li>로 추가
 for img in images/*; do
   filename=$(basename "$img")
-  echo "  <li>$filename</li>" >> index.html
+  echo "    <li>$filename</li>" >> index.html
 done
 
+# </ul> 및 </body> 닫기
 cat >> index.html <<EOF
   </ul>
 </body>
 </html>
-EOF 
+EOF
